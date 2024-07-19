ce9cb5187107c0f893f9d786bb97aa0652e7a200
hau <nguyenhau1720@gmail.com>
2024-07-19 10:40:32 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-document.getElementById('myButton').addEventListener('click', function() {
-    alert('Button was clicked!');
-});
 function redirectToOtherPage(page) {
     window.location.href = page;
 }
+
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let lengthItems = items.length - 1;
+let active = 0;
+
+function next() {
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+
+function prev() {
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+
+let refreshInterval = setInterval(next, 5000);
+
+function reloadSlider() {
+    slider.style.left = -items[active].offsetLeft + 'px';
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(next, 5000);
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', () => {
+        active = key;
+        reloadSlider();
+    });
+});
+
+reloadSlider();
